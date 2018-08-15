caa07bfb14b74413cf4ccc7104c0477f3d086ef0
Brad McCollum <btmcco8@gmail.com>
2018-08-15 15:59:16 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -86,10 +86,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-# programmer_hash[:yukihiro_matsumoto]
-# programmer_hash[:yukihiro_matsumoto] = {}
-programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-programmer_hash
+return programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+
 end
 
 def changing_alan
